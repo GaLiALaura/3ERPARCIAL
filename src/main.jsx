17667755beae0b068a9ef3f4ba7e2524d7338083
GaLiALaura <GaLiaLaurarojasgalia777@gmail.com>
2024-06-20 10:17:04 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import ClasesDisponibles from './ClasesDisponibles/ClasesDisponibles'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClasesDisponibles />
+  </React.StrictMode>,
+)
